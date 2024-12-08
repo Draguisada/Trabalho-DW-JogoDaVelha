50fbba3ef3e19aca71333eafee3b25766837e314
Draguisada <draguisada@gmail.com>
2024-12-08 19:06:53 -0300

    Checar vitóra add

--- script/jogos/normal.js
+++ script/jogos/normal.js
@@ -2,12 +2,15 @@ function celulaClick(actor) {
     if (actor.textContent == ""){
     actor.textContent = jogador[jogador[0]];
     jogador[0] = jogador[0]-1 ? 1 : 2;
-}
-}
-
+    verJogar.textContent = jogador[jogador[0]];
+    round+=1;
+    if (checkWin('normal')) {winGame(); return;};
+    if (round >= 9) {
+        drawGame();
+    }
+}}
 
 if (jogo == 1) {
-
 
 celula = document.querySelector('#normal jogo');
 celula.addEventListener('click', event => celulaClick(event.target));
--- script/main.js
+++ script/main.js
@@ -1,6 +1,70 @@
+// Variaveis e Const do código
 let jogo;
 jogo = 1;
 let jogador = [1, 'X', 'O'];
 let celula;
+let round = 0;
 
+const verJogar = document.querySelector('ui strong');
 const jogoNormal = document.getElementById('normal');
+
+
+// Funções globais
+function getJogo(string) {
+    let jogo;
+    if (string == 'normal') {
+        jogo = document.querySelector('#normal jogo');
+    } 
+    else if (string == 'triangular') {
+        jogo = document.querySelector('#triangular jogo');
+    }
+    else if (string == 'super') {
+        jogo = document.querySelector('#super jogo');
+    }
+    return jogo
+}
+
+function clearBoard(string) {
+    let jogo = getJogo(string);
+
+    let crionca = jogo.children;
+    for (i=0;  i<crionca.length  ;i++) {
+        crionca[i].textContent = '';
+    }
+}
+
+function checkWin(string) {
+    if (round<5) {
+        return false;
+    }
+    let jogo = getJogo(string);
+    let crionca = jogo.children;
+
+    if (crionca[0].textContent == crionca[8].textContent && crionca[0].textContent == crionca[4].textContent && crionca[0].textContent != '') {
+        return true;
+    }
+
+    if (crionca[2].textContent == crionca[4].textContent && crionca[2].textContent == crionca[6].textContent && crionca[2].textContent != '') {
+        return true;
+    }
+
+    for (var i=0; i<3; i++) {
+    for (let j=0; j<3; j++) {
+        if (crionca[j].textContent == crionca[j+3].textContent && crionca[j+3].textContent == crionca[j+6].textContent && crionca[j].textContent != '') {
+            return true;
+        }
+
+        if (crionca[j+i*3].textContent == crionca[j+i*3+1].textContent && crionca[j+i*3+1].textContent == crionca[j+i*3+2].textContent && crionca[j+i*3].textContent != '') {
+            return true;    
+        }
+    }
+    }
+}
+
+function drawGame() {
+    alert('jogo empatado');
+}
+
+function winGame() {
+    alert('jogo ganho');
+}
